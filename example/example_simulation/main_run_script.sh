feb9b85fb2faa7b8b5dd8479935229d09b994563
HMiras <hector.miras@gmail.com>
2023-10-29 00:51:16 +0200

    Added example and documentation folder

---
+++ example/example_simulation/main_run_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INFILE1="I125_seed_in_water.txt" 
+
+CURRENTPATH=`pwd`
+SEED=`bash -c 'echo $RANDOM'`
+sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $CURRENTPATH/$INFILE1
+# Topas path in docker container:
+# time /topas/bin/topas $CURRENTPATH/$INFILE1
+# Local topas path
+time ~/topas/bin/topas $CURRENTPATH/$INFILE1
+	
